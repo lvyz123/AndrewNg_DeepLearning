50a539efe141776a36b71f096c4f078892b6e202
lvyz123 <35937287+lvyz123@users.noreply.github.com>
2019-04-12 15:57:29 -0400

    Add files via upload

---
+++ Keras_Tutorial.py
@@ -0,0 +1,82 @@
+import numpy as np
+from keras import layers
+from keras.layers import Input, Dense, Activation, ZeroPadding2D, BatchNormalization, Flatten, Conv2D
+from keras.layers import AveragePooling2D, MaxPooling2D, Dropout, GlobalMaxPooling2D, GlobalAveragePooling2D
+from keras.models import Model
+from keras.preprocessing import image
+from keras.utils import layer_utils
+from keras.utils.data_utils import get_file
+from keras.applications.imagenet_utils import preprocess_input
+import pydot
+from IPython.display import SVG
+from keras.utils.vis_utils import model_to_dot
+from keras.utils import plot_model
+from kt_utils import *
+
+import keras.backend as K
+K.set_image_data_format('channels_last')
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import imshow
+
+%matplotlib inline
+
+# GRADED FUNCTION: HappyModel
+
+def HappyModel(input_shape):
+    """
+    Implementation of the HappyModel.
+
+    Arguments:
+    input_shape -- shape of the images of the dataset
+
+    Returns:
+    model -- a Model() instance in Keras
+    """
+
+    ### START CODE HERE ###
+    # Feel free to use the suggested outline in the text above to get started, and run through the whole
+    # exercise (including the later portions of this notebook) once. The come back also try out other
+    # network architectures as well.
+    # Define the input placeholder as a tensor with shape input_shape. Think of this as your input image!
+    input_shape = (input_shape[1], input_shape[2], input_shape[3])
+    X_input = Input(shape=input_shape)
+
+    # Zero-Padding: pads the border of X_input with zeroes
+    X = ZeroPadding2D((3, 3))(X_input)
+
+    # CONV -> BN -> RELU Block applied to X
+    X = Conv2D(32, (3, 3), strides = (1, 1), name = 'conv0')(X)
+    X = BatchNormalization(axis = 3, name = 'bn0')(X)
+    X = Activation('relu')(X)
+
+    X = AveragePooling2D((2, 2), name='ave_pool_0')(X)
+
+    X = Conv2D(64, (3, 3), strides = (1, 1), name = 'conv1')(X)
+    X = BatchNormalization(axis = 3, name = 'bn1')(X)
+    X = Activation('relu')(X)
+
+    # MAXPOOL
+    X = AveragePooling2D((2, 2), name='ave_pool_1')(X)
+
+    # FLATTEN X (means convert it to a vector) + FULLYCONNECTED
+    X = Flatten()(X)
+    X = Dense(1, activation='sigmoid', name='fc')(X)
+
+    # Create model. This creates your Keras model instance, you'll use this instance to train/test the model.
+    model = Model(inputs = X_input, outputs = X, name='HappyModel')
+
+    ### END CODE HERE ###
+
+    return model
+
+### START CODE HERE ### (1 line)
+happyModel = HappyModel(X_train.shape)
+### END CODE HERE ###
+
+### START CODE HERE ### (1 line)
+happyModel.compile(optimizer = "Adam", loss = "binary_crossentropy", metrics = ["accuracy"])
+### END CODE HERE ###
+
+### START CODE HERE ### (1 line)
+happyModel.fit(x = X_train, y = Y_train, epochs = 1, batch_size = 24)
+### END CODE HERE ###
